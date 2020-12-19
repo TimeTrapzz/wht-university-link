e890d351073ff7c3a6501dd93e35af4a56326969
TimeTrapzz <55572425+TimeTrapzz@users.noreply.github.com>
2020-12-19 22:20:21 +0800

    Add HUST

---
+++ univ/11002.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "华中科技大学",
+    id: "11002",
+    link: [ {
+        name: "华科主页",
+        url: "https://www.hust.edu.cn/"
+    }, {
+        name: "hub系统",
+        url: "http://hub.hust.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.hust.edu.cn/"
+    }, {
+        name: "邮箱",
+        url: "https://mail.hust.edu.cn/"
+    }, {
+        name: "教务处",
+        url: "http://jwc.hust.edu.cn/"
+    }, {
+        name: "VPN",
+        url: "https://vpn.hust.edu.cn/"
+    }, {
+        name: "网络计算中心",
+        url: "http://ncc.hust.edu.cn/"
+    } ]
+};
